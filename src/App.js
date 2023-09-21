990b582bb972bdec1222c02feaf7ba9ae9a0a8c7
Owuraku <owurakusintim@gmail.com>
2023-09-21 17:47:39 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ClassForm from "./components/ClassForm";
+import FunctionalForm from "./components/FunctionalForm";
+
+function App() {
+  return (
+    <div className="App">
+      <ClassForm />
+      <br /> <br />
+      <FunctionalForm />
+    </div>
+  );
+}
+
+export default App;
